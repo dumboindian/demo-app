deccedccc462cb59a26e7e230cdbba0863980a62
dumboindian <5023156@it.fcrit.ac.in>
2026-03-24 09:19:02 +0000

    New committt

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,17 +1,17 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
     @Test
-    void testRegexValid() {
+    public void testValidUsername() {
         assertTrue("user123".matches("^\\w{3,20}$"));
     }
 
     @Test
-    void testRegexInvalid() {
+    public void testInvalidUsername() {
         assertFalse("ab".matches("^\\w{3,20}$"));
     }
 }
